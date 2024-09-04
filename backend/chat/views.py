9884670b89c6e50d531105a560a0b336661b1757
prajun v <prajunvelandi@gmail.com>
2024-09-04 13:08:05 +0530

    first commit

---
+++ backend/chat/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Message, Room
+
+# Create your views here.
+def HomeView(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        room = request.POST["room"]
+        print(username, room)
+        try:
+            existing_room = Room.objects.get(room_name__icontains=room)
+        except Room.DoesNotExist:
+            r = Room.objects.create(room_name=room)
+        return redirect("room", room_name=room, username=username)
+    return render(request, 'home.html')
+
+def RoomView(request, room_name, username):
+    print(room_name, username)
+    existing_room = Room.objects.get(room_name__icontains=room_name)
+    get_messages = Message.objects.filter(room=existing_room)
+    print(get_messages)
+    context = {
+        "messages": get_messages,
+        "user": username,
+        "room_name": existing_room.room_name,
+    }
+    return render(request, 'room.html', context)
---
+++ readme.md
@@ -0,0 +1,3 @@
+I was able to successfully implement chat in django using channels
+Thanks to : https://youtu.be/mhADNEyMBis?si=YLPiSN3CWbfL2hei
+
